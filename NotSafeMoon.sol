8d722cfda27c3248e890fdde38a9b6ccbe263790
NotSafeMoon <82337996+NotSafeMoon@users.noreply.github.com>
2021-05-03 19:10:38 -0700

    update to pancake swap v2 router

--- NotSafeMoon.sol
+++ NotSafeMoon.sol
@@ -260,7 +260,8 @@ contract NotSafeMoon is Context, IERC20, Ownable, ReentrancyGuard {
 
 	constructor () {
 		_rOwned[_msgSender()] = _rTotal;
-		IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);		// binance
+		IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);		// binance PANCAKE V2
+		//IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);		// binance PANCAKE V1
 		//IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);		// Ethereum mainnet, Ropsten, Rinkeby, Görli, and Kovan		 
 		uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory()).createPair(address(this), _uniswapV2Router.WETH());
 		uniswapV2Router = _uniswapV2Router;
